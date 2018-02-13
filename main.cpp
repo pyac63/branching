e82fad99a3d0b34b539c8b5a493706dc0064a27b
Antoine <antoine.camboly@gmail.com>
2018-02-13 12:44:23 +0100

    ajout fonction add et print

--- main.cpp
+++ main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+int add (int x, int y)
+{
+    return x+y;
+}
+
+void print()
+{
+    cout << "Enter a number: \n";
+    int x;
+    cin >>x;
+    cout << "Enter 2nd number: \n";
+    int y; cin >> y;
+    cout << "Addition of both is: " << add(x, y) << "\n";
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
+    print();
     return 0;
 }
